de19dad3b66d4cab8fcec84ad4ddedabeba74d65
Ryotaro Kato <ryo.ctuad@gmail.com>
2018-03-07 15:46:26 +0900

    [ADD] all hand

--- src/components/part/Clock.vue
+++ src/components/part/Clock.vue
@@ -1,7 +1,7 @@
 <template lang='pug'>
 .vue-clock
-    .hand#hour-hand: div(ref='hours')
-    .hand#minute-hand: div(ref='minutes')
+    .hand#hour-hand: div(ref='hours') {{ updateHours() }}
+    .hand#minute-hand: div(ref='minutes') {{ updateMinutes() }}
     .hand#second-hand: div(ref='seconds') {{ updateMilliseconds() }}
     .center: div
 
@@ -26,7 +26,7 @@ export default class Clock extends Vue {
         }, 30);
     }
 
-    protected setRotate(elem: HTMLElement, deg: number) {
+    protected setRotate(elem: HTMLElement, deg: number) {          //要素の指示　 deg=角度つまり、角度を数字で表すように指示
         if (elem == null) {
             return;
         }
@@ -40,6 +40,23 @@ export default class Clock extends Vue {
         const elem = this.$refs.seconds as HTMLElement;
         const deg = (360 * milliSeconds) / 60000;
         this.setRotate(elem, deg)
+    }
+
+    protected updateMinutes(): void {
+        const minutes = this.date.getMinutes() * 1000
+        + this.date.getMinutes();
+        const elem = this.$refs.minutes as HTMLElement;
+        //$ref 子コンポーネント参照できる
+        const deg = (360 * minutes) / 60000;
+        this.setRotate(elem , deg)
+    }
+
+    protected updateHours(): void{
+        const hours = this.date.getHours() * 1000
+        + this.date.getHours();
+        const elem = this.$refs.hours as HTMLElement;
+        const deg = (360 * hours) / 60000;
+        this.setRotate(elem , deg)
     }
 }
 </script>
@@ -76,12 +93,13 @@ export default class Clock extends Vue {
         position: absolute
         height: 100%
         width: 100%
+        border-radius: 50%
 
         & > div
             background-color: black
             position: absolute
-            width: 16px
-            height: 16px
+            width: 30px
+            height: 30px
             top: 50%
             left: 50%
             transform: translate(-50%, -50%)
